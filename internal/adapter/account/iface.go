dedafd17097e609fc63f04bd632ab761145a5706
Rodrigo Prata <rodrigo-p-braga@telecom.pt>
2023-07-16 11:27:14 +0100

    create controller interface

--- internal/adapter/account/iface.go
+++ internal/adapter/account/iface.go
@@ -1,7 +1,14 @@
 package account
 
-import "silver-clean-code/internal/entity"
+import (
+	"silver-clean-code/internal/adapter"
+	"silver-clean-code/internal/entity"
+)
 
 type UseCase interface {
 	GetByID(id uint64) (*entity.Account, error)
+}
+
+type Controller interface {
+	FindByID(ctx adapter.ContextServer) error
 }
