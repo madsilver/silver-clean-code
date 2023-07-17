efc9e88d169ba3189d539f38715a90d2ea4ead04
Rodrigo Prata <rodrigo-p-braga@telecom.pt>
2023-07-17 01:18:01 +0100

    improve test coverage

--- internal/usecase/account/usecase_test.go
+++ internal/usecase/account/usecase_test.go
@@ -50,3 +50,42 @@ func Test_accountUseCase_GetByID(t *testing.T) {
 		})
 	}
 }
+
+func TestAccountUseCase_GetAccounts(t *testing.T) {
+	tests := []struct {
+		name    string
+		want    []*entity.Account
+		wantErr bool
+	}{
+		{
+			name:    "should return a list of accounts",
+			want:    []*entity.Account{{AccountID: 1}},
+			wantErr: false,
+		},
+		{
+			name:    "should return an error",
+			want:    nil,
+			wantErr: true,
+		},
+	}
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	repository := mockaccount.NewMockRepository(ctrl)
+	gomock.InOrder(
+		repository.EXPECT().FindAll().Return([]*entity.Account{{AccountID: 1}}, nil),
+		repository.EXPECT().FindAll().Return(nil, errors.New("error")),
+	)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := NewAccountUseCase(repository)
+			got, err := a.GetAccounts()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAccounts() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetAccounts() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
