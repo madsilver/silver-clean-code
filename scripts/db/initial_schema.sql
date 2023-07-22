d3770f44de57376416a85f5754af26a8c1969ef8
Rodrigo Prata <rodrigo-p-braga@telecom.pt>
2023-07-22 08:11:38 +0100

    add transaction schema

--- scripts/db/initial_schema.sql
+++ scripts/db/initial_schema.sql
@@ -5,4 +5,14 @@ CREATE TABLE `Account` (
     `AccountID` int NOT NULL AUTO_INCREMENT,
     `DocumentNumber` varchar(20),
     PRIMARY KEY (`AccountID`)
-)
+);
+
+DROP TABLE IF EXISTS `Transaction`;
+CREATE TABLE `Transaction` (
+    `TransactionID` int NOT NULL AUTO_INCREMENT,
+    `AccountID` int,
+    `OperationTypeID` int,
+    `Amount` decimal(15,2),
+    `EventDate` datetime(5),
+    PRIMARY KEY (`TransactionID`)
+);
